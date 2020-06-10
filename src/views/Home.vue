40dea15f793505bdb0abf6998c7eedc3a5aa3013
Hatsanai <kongsanais@gmail.com>
2020-06-10 18:11:46 +0700

    new version

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,5 +1,32 @@
 <template>
   <div class="home">
-    <h1>Home</h1>
+    <v-container >
+      <v-row  class="d-flex flex-row align-center" v-for="(item, index) in todos" :key="index">
+        <v-checkbox  v-model="value" value="value"></v-checkbox>
+        <span>{{item.title}}</span>
+        <v-spacer></v-spacer>
+        <v-btn color="error" >x</v-btn>
+      </v-row>
+    </v-container>
   </div>
 </template>
+
+
+
+<script>
+export default {
+  name:"home",
+  components:{
+
+  },
+  data(){
+    return{
+     todos:[
+       {id: 1 , title:"Task 1", completed:false},
+       {id: 2 , title:"Task 2", completed:true},
+       {id: 3 , title:"Task 3", completed:false},
+     ]
+    }
+  },
+};
+</script>
