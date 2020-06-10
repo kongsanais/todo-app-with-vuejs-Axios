d9432c6b227048ce4f05918d68777244c0fd4fdd
Hatsanai <kongsanais@gmail.com>
2020-06-10 18:30:46 +0700

    try

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,12 +1,53 @@
 <template>
   <div class="home">
     <v-container >
-      <v-row  class="d-flex flex-row align-center" v-for="(item, index) in todos" :key="index">
+  
+   <v-card
+    class="mx-auto"
+    color="green"
+    dark
+    max-width="800"
+    >
+    <v-card-title> 
+    <v-icon large color="blue darken-2" style="margin-right:10px">mdi-message-text</v-icon>
+
+      <span class="title font-weight-light">List of Task</span>
+    </v-card-title>
+
+    <v-card-text class="headline font-weight-bold">
+              <v-row  class="d-flex flex-row align-center" v-for="(item, index) in todos" :key="index">
         <v-checkbox  v-model="value" value="value"></v-checkbox>
         <span>{{item.title}}</span>
         <v-spacer></v-spacer>
         <v-btn color="error" >x</v-btn>
       </v-row>
+    </v-card-text>
+
+    <v-card-actions>
+      <v-list-item class="grow">
+
+        <v-list-item-content>
+          <v-img
+            class="elevation-6"
+            src="https://avataaars.io/?avatarStyle=Transparent&topType=ShortHairShortCurly&accessoriesType=Prescription02&hairColor=Black&facialHairType=Blank&clotheType=Hoodie&clotheColor=White&eyeType=Default&eyebrowType=DefaultNatural&mouthType=Default&skinColor=Light"
+          >
+          </v-img>
+          <v-list-item-title>Hatsanai</v-list-item-title>
+        </v-list-item-content>
+
+        <v-row
+          align="center"
+          justify="end"
+        >
+          <span class="mr-2">Total : </span>
+          <span class="subheading mr-2">256</span>
+        </v-row>
+
+        
+      </v-list-item>
+    </v-card-actions>
+  </v-card>
+
     </v-container>
   </div>
 </template>
